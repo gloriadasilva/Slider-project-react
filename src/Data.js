8979ce39423de6b01a1979a93618b25aa74c2850
Gloria <gloriadasilva620@gmail.com>
2024-06-12 10:03:27 +0100

    I changed the images

--- src/Data.js
+++ src/Data.js
@@ -1,30 +1,31 @@
 
+const baseURL = "/Slider-project-react";
 
 const Data = [
   {
       id : 1,
-      img : "/assets/imageOne.jpg",
+      img : baseURL + "/assets/imageOne.jpg",
       name : "Susan Hannah",
       job : "UI/ UX",
       quote : "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi suscipit vel animi aut deleniti odit?",
   },
   {
       id : 2,
-      img : "/assets/imageTwo.jpg",
+      img : baseURL + "/assets/imageTwo.jpg",
       name : "Alexandra Stacey",
       job : "Visual Designer",
       quote : "Daniels favourite quote ipsum dolor sit amet consectetur adipisicing elit. Praesentium saepe dolorum eligendi voluptates amet. Explicabo corporis possimus aliquam modi laborum."
   },
   {
       id : 3,
-      img : "/assets/imageThree.jpg",
+      img :baseURL +  "/assets/imageThree.jpg",
       name : "Daniels brown",
       job : "Developer",
       quote : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium saepe dolorum eligendi voluptates amet. Explicabo corporis possimus aliquam modi laborum."
   },
   {
       id : 4,
-      img : "/assets/imageFour.jpg",
+      img : baseURL+ "/assets/imageFour.jpg",
       name : "Olivia Trenor",
       job : "Digital Marketer",
       quote : "The olive tree Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium saepe dolorum eligendi voluptates amet. Explicabo corporis possimus aliquam modi laborum."
